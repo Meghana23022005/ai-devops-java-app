90c9173dea5cd57d005e062b9c0437ec770aa26b
Meghana23022005 <meghana.bcm23@gmail.com>
2026-02-12 18:10:16 +0530

    Add BadCodeDemo class with divide method

---
+++ src/main/java/com/devops/demo/AppLogin.java
@@ -0,0 +1,17 @@
+package com.devops.demo;
+
+public class BadCodeDemo {
+
+    
+    private static final String PASSWORD = "admin123";
+
+    public int divide(int a, int b) {
+        
+        return a / b;
+    }
+
+    public void unusedMethod() {
+        
+        int x = 10;
+    }
+}
